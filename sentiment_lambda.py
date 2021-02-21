1fc1174e233582291c84a448a0e43d0dcd92a8df
ThickLimes <77542388+ThickLimes@users.noreply.github.com>
2021-02-21 09:56:45 -0600

    Update sentiment_lambda.py

--- sentiment_lambda.py
+++ sentiment_lambda.py
@@ -14,28 +14,22 @@ def lambda_sentiment(event, context):
         dict_data = base64.b64decode(record['data']).decode('utf-8').strip()
         print(dict_data)
         
-        comprehend = boto3.clinanoent(service_name='comprehend', region_name='eu-west-1')
-        sentiment_all = comprehend.detect_sentiment(Text=dict_data, LanguageCode='en')
-        sentiment = sentiment_all['Sentiment']
-        print(sentiment)
-        positive = sentiment_all['SentimentScore']['Positive']
-        negative = sentiment_all['SentimentScore']['Negative']
-        total = positive - negative
-        print(total)
+        comprehend = boto3.client(service_name='comprehend', region_name='eu-west-1')
+        sent = comprehend.detect_sentiment(Text=dict_data, LanguageCode='en')
+        sentiment = sent['Sentiment']
+        pos = sent['SentimentScore']['Positive']
+        neg = sent['SentimentScore']['Negative']
+        total = pos - neg
         
         data_record = {
             'message': dict_data,
             'sentiment': sentiment,
-            'total': total
-        }
-        print(data_record)
-        
+            'total': total}
+ 
         output_record = {
             'recordId': record['recordId'],
             'result': 'Ok',
-            'data': base64.b64encode(json.dumps(data_record).encode('utf-8')).decode('utf-8')
-        }
-        print(output_record)
+            'data': base64.b64encode(json.dumps(data_record).encode('utf-8')).decode('utf-8')}
         
         output.append(output_record)
 
